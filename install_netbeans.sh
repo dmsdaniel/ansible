df966bc87d30e7621101f7ee4d4f646f56de632c
daniel santos <daniel@wareline.com.br>
2019-07-12 16:21:55 -0300

    new file:   install_netbeans.sh

---
+++ install_netbeans.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+URL=https://www-us.apache.org/dist/incubator/netbeans/incubating-netbeans/incubating-10.0/incubating-netbeans-10.0-bin.zip
+
+# install required applications
+sudo apt -y install wget unzip
+
+# download netbeans package
+wget ${URL} -O /tmp/incubating-netbeans-bin.zip
+
+# unip netbeans
+sudo unzip -d /opt /tmp/incubating-netbeans-bin.zip
+
+# create netbeans starter
+cat <<EOF> /tmp/netbeans.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=NetBeans IDE
+Exec=/opt/netbeans/bin/netbeans
+Icon=/opt/netbeans/nb/netbeans.png
+Terminal=false
+Type=Application
+Categories=GNOME;Application;Development;
+EOF
+
+sudo mv /tmp/netbeans.desktop /usr/share/applications/netbeans.desktop
+sudo chmod +r /usr/share/applications/netbeans.desktop
