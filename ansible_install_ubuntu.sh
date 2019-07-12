b96a68a28df00d43f748ad4ecc722fad68690936
daniel santos <daniel@wareline.com.br>
2019-07-12 15:56:00 -0300

    modified:   ansible_install_ubuntu.sh

--- ansible_install_ubuntu.sh
+++ ansible_install_ubuntu.sh
@@ -4,3 +4,4 @@ sudo apt-get --assume-yes install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get --assume-yes update 
 sudo apt-get --assume-yes install ansible
+sudo ansible-pull -U https://github.com/dmsdaniel/ansible.git
