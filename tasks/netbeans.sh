ec8a40a712a4c38d0c73cd14b81da3447d2cdb29
daniel santos <daniel@wareline.com.br>
2019-07-12 16:31:33 -0300

    modified:   netbeans.sh

--- tasks/netbeans.sh
+++ tasks/netbeans.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
 
-URL=https://www-us.apache.org/dist/incubator/netbeans/incubating-netbeans/incubating-10.0/incubating-netbeans-10.0-bin.zip
+#!/bin/bash
+URL=https://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-javaee-linux.sh
+
+#URL=https://www-us.apache.org/dist/incubator/netbeans/incubating-netbeans/incubating-10.0/incubating-netbeans-10.0-bin.zip
 
 # install required applications
 sudo apt -y install wget unzip
 
 # download netbeans package
-wget ${URL} -O /tmp/incubating-netbeans-bin.zip
+wget ${URL} -O /tmp/netbeans-8.2-javaee-linux.sh
 
 # unip netbeans
-sudo unzip -d /opt /tmp/incubating-netbeans-bin.zip
+#sudo unzip -d /opt /tmp/incubating-netbeans-bin.zip
+sh /tmp/netbeans-8.2-javaee-linux.sh
 
 # create netbeans starter
 cat <<EOF> /tmp/netbeans.desktop
