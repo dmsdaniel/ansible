2a2cc5b6f7db1dcebb47fd3258fe3e96bee75ee7
Daniel <dmsdaniel@gmail.com>
2019-07-12 17:02:06 -0300

    modified:   tasks/java.sh

--- tasks/java.sh
+++ tasks/java.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/{$USER}/
-tar xvzf  jre-8u171-linux-x64.tar.gz
+tar xvzf  ../src/jre-8u171-linux-x64.tar.gz -C /home/$USER
+echo 'export JAVA_HOME=/home/$USER/jre1.8.0_171' >> ~/.profile
+echo 'export PATH=/home/$USER/jre1.8.0_171/bin:$PATH' >> ~/.profile
+source ~/.profile
